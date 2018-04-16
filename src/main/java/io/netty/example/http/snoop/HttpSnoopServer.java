b3e54d0bd9fdd36c4f01872ae432d45acb374f4a
parkerwu2 <982051137@qq.com>
2018-04-16 10:45:28 +0800

    earn from offical net

---
+++ src/main/java/io/netty/example/http/snoop/HttpSnoopServer.java
@@ -0,0 +1,71 @@
+/*
+2    * Copyright 2012 The Netty Project
+3    *
+4    * The Netty Project licenses this file to you under the Apache License,
+5    * version 2.0 (the "License"); you may not use this file except in compliance
+6    * with the License. You may obtain a copy of the License at:
+7    *
+8    *   http://www.apache.org/licenses/LICENSE-2.0
+9    *
+10   * Unless required by applicable law or agreed to in writing, software
+11   * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+12   * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+13   * License for the specific language governing permissions and limitations
+14   * under the License.
+15   */
+package io.netty.example.http.snoop;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+/**
+ * 30   * An HTTP server that sends back the content of the received HTTP request
+ * 31   * in a pretty plaintext form.
+ * 32
+ */
+public final class HttpSnoopServer {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+        } else {
+            sslCtx = null;
+        }
+
+        // Configure the server.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpSnoopServerInitializer(sslCtx));
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            System.err.println("Open your web browser and navigate to " +
+                    (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
---
+++ src/main/java/io/netty/example/http/snoop/HttpSnoopServerInitializer.java
@@ -0,0 +1,50 @@
+
+
+/*
+2    * Copyright 2012 The Netty Project
+3    *
+4    * The Netty Project licenses this file to you under the Apache License,
+5    * version 2.0 (the "License"); you may not use this file except in compliance
+6    * with the License. You may obtain a copy of the License at:
+7    *
+8    *   http://www.apache.org/licenses/LICENSE-2.0
+9    *
+10   * Unless required by applicable law or agreed to in writing, software
+11   * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+12   * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+13   * License for the specific language governing permissions and limitations
+14   * under the License.
+15   */
+package io.netty.example.http.snoop;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpSnoopServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpSnoopServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpRequestDecoder());
+        // Uncomment the following line if you don't want to handle HttpChunks.
+        //p.addLast(new HttpObjectAggregator(1048576));
+        p.addLast(new HttpResponseEncoder());
+        // Remove the following line if you don't want automatic content compression.
+        //p.addLast(new HttpContentCompressor());
+        p.addLast(new HttpSnoopServerHandler());
+    }
+}
+
